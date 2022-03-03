d1ca898c9b4cf4ecf259367bcc6b66e7fcadb4ae
Gumeece <81268663+Gumeece@users.noreply.github.com>
2022-03-03 11:25:51 -0700

    added the math

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from dataclasses import dataclass
 from locale import currency
 from msilib.schema import Error
 from tkinter import *   
 import random
-import math
+import math 
 from mysqlx import DatabaseError
 from getDatabase import getDatabase
 import mysql.connector
@@ -10,23 +11,39 @@ try:
     db_connection = mysql.connector.connect(host='localhost',user='root',password="1234",database='chatbot')
 except DatabaseError: 
     print(DatabaseError)
-
+def equation(x):
+        y = (-1*(abs((x-1)/(math.e*(math.sqrt(1+pow(x-1,2)))))))+(math.e/10)
+        return y
+       
 win = Tk()
 win.geometry('400x170')
 class ChatbotGUI():
-    def __init__(self) -> None:
+    def __init__(self):
         self.current_word=None
         self.database = getDatabase(db_connection)
         self.words_id = self.database.getWordsID()
         self.words_factor = self.database.getWords()
+
     def no(self):
         try:
-            print(f'No: {self.current_word}, {self.words_factor[self.current_word]}')
+            x = self.words_factor[self.current_word]
+            y = ((1-equation(x))*(x-.1))
+            if y >= 2 or y <= 0:
+                print(f'Stop: {self.current_word}, {x}')
+            else:
+                self.words_factor[self.current_word] = y
+                print(f'No: {self.current_word}, {self.words_factor[self.current_word]}')
         except KeyError:
             print("No current word")
     def yes(self):
         try:
-            print(f'Yes: {self.current_word}, {self.words_factor[self.current_word]}')
+            x = self.words_factor[self.current_word]
+            y = ((1+equation(x))*(x+.1))
+            if y >= 2 or y <= 0:
+                print(f'Stop: {x}')
+            else:
+                self.words_factor[self.current_word] = y
+                print(f'Yes: {self.current_word}, {self.words_factor[self.current_word]}')
         except KeyError:
             print("No current word")
     def giveWord(self):
@@ -42,4 +59,5 @@ yes_button.pack(side='top')
 no_button.pack(side='top')
 again_button.pack(side='top')
 win.mainloop()  
+#use /4 to find percentages
 # print(math.log(100,10))
