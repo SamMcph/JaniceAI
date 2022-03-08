80ee9c95ad81cc6c70c8679a7ee46f3e4f3503ec
Gumeece <81268663+Gumeece@users.noreply.github.com>
2022-03-08 10:57:25 -0700

    added goodbye intent

--- getDatabase.py
+++ getDatabase.py
@@ -2,30 +2,42 @@ from cgitb import reset
 from logging.handlers import MemoryHandler
 import re
 from unittest import result
-
-
 class getDatabase():
-    def __init__(self,db_connection) -> None:
-        self.table = 'SELECT * FROM chatbot_data.greetings;'
+    def __init__(self,db_connection,table) -> None:
+        self.dbconnection = db_connection
+        self.table = table 
         self.my_database = db_connection.cursor()
     def getTable(self):
-        self.my_database.execute(self.table)
+        self.my_database.execute(f'SELECT * FROM {self.table};')
         result = self.my_database.fetchall()
         rows = []
         for row in result:
             rows.append(row)
         return rows
     def getWords(self):
-        self.my_database.execute(self.table)
+        self.my_database.execute(f'SELECT * FROM {self.table};')
         result = self.my_database.fetchall()
         words = {}
         for row in result:
             words[row[3]]= row[4]
         return(words)
     def getWordsID(self):
-        self.my_database.execute(self.table)
+        self.my_database.execute(f'SELECT * FROM {self.table};')
         result = self.my_database.fetchall()
         word_id = {}
         for row in result:
             word_id[row[1]] = row[3]
         return word_id  
+    def getIDWords(self):
+        self.my_database.execute(f'SELECT * FROM {self.table};')
+        result = self.my_database.fetchall()
+        id_words = {}
+        for row in result:
+            id_words[row[3]] = row[1]
+        return id_words
+    def updateValue(self, id, factor):
+        query = f"UPDATE {self.table} SET factor ='{str(factor)}' WHERE wordId='{str(id)}'"
+        self.my_database.execute(str(query))
+        with self.dbconnection.cursor() as cursor:
+            cursor.execute(query)
+            self.dbconnection.commit()
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
+from gc import get_referents
 from locale import currency
 from msilib.schema import Error
 from tkinter import *   
 import random
-import math 
+import math
+from webbrowser import get 
 from mysqlx import DatabaseError
 from getDatabase import getDatabase
 import mysql.connector
@@ -14,43 +16,62 @@ except DatabaseError:
 def equation(x):
         y = (-1*(abs((x-1)/(math.e*(math.sqrt(1+pow(x-1,2)))))))+(math.e/10)
         return y
-       
 win = Tk()
 win.geometry('400x170')
 class ChatbotGUI():
     def __init__(self):
         self.current_word=None
-        self.database = getDatabase(db_connection)
+        self.database = getDatabase(db_connection,'chatbot_data.greetings')
         self.words_id = self.database.getWordsID()
         self.words_factor = self.database.getWords()
-
+        self.IDwords = self.database.getIDWords()
+        self.find_percent = {}
+        for i in self.words_factor:
+            percent = (self.words_factor[i]/4)*100
+            self.find_percent[i] = percent
     def no(self):
         try:
             x = self.words_factor[self.current_word]
-            y = ((1-equation(x))*(x-.1))
+            y = ((1-equation(x))*(x-.05))
             if y >= 2 or y <= 0:
-                print(f'Stop: {self.current_word}, {x}')
+                print(f'Stop: {self.IDwords[self.current_word]}, {x}')
+                self.database.updateValue(self.IDwords[self.current_word],x)
+                percent = (x/4)*100
+                self.find_percent[self.current_word]=int(percent)
             else:
                 self.words_factor[self.current_word] = y
+                self.database.updateValue(self.IDwords[self.current_word],y)
                 print(f'No: {self.current_word}, {self.words_factor[self.current_word]}')
+                percent = (y/4)*100
+                self.find_percent[self.current_word]=int(percent)
         except KeyError:
             print("No current word")
     def yes(self):
         try:
             x = self.words_factor[self.current_word]
-            y = ((1+equation(x))*(x+.1))
+            y = ((1+equation(x))*(x+.05))
             if y >= 2 or y <= 0:
                 print(f'Stop: {x}')
+                self.database.updateValue(self.IDwords[self.current_word],x)
+                percent = (x/4)*100
+                self.find_percent[self.current_word]=int(percent)
             else:
                 self.words_factor[self.current_word] = y
                 print(f'Yes: {self.current_word}, {self.words_factor[self.current_word]}')
+                self.database.updateValue(self.IDwords[self.current_word],y)
+                percent = (y/4)*100
+                self.find_percent[self.current_word]=int(percent)
         except KeyError:
             print("No current word")
     def giveWord(self):
-        random_num = random.randint(0,7)
-        random_word = self.words_id[random_num]
-        print(f'Computer: {random_word}: {self.words_factor[random_word]}')
-        self.current_word = random_word
+        my_percents = []
+        my_words= []
+        for i in self.find_percent:
+            my_percents.append(self.find_percent[i])
+            my_words.append(i)
+        random_word = random.choices(my_words,weights=my_percents)
+        print(f'Computer: {random_word[0]}: {self.words_factor[random_word[0]]}')
+        self.current_word = random_word[0]
 chatbotGUI = ChatbotGUI()
 yes_button = Button(win, text="Yes", bd=5,width=100,height=2,fg='red', command=chatbotGUI.yes)
 no_button = Button(win,text='No',bd=5,width=100,height=2, fg='blue',command=chatbotGUI.no)
@@ -59,5 +80,7 @@ yes_button.pack(side='top')
 no_button.pack(side='top')
 again_button.pack(side='top')
 win.mainloop()  
+my_databse = getDatabase(db_connection, 'SELECT * FROM chatbot_data.goodbye;')
+print(my_databse.test())
 #use /4 to find percentages
 # print(math.log(100,10))
