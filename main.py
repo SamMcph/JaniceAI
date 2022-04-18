86e80315b8bd82e64ff44c7f4090f56fb2efa878
Gumeece <81268663+Gumeece@users.noreply.github.com>
2022-04-18 10:30:38 -0600

    added skills

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from tkinter import *  
 import random
 import math
+from turtle import color
+from urllib.request import urlopen
 from mysqlx import DatabaseError
 from getDatabase import getDatabase
 import mysql.connector
 import json
+from skill import news, weather, time,date
 try: 
     db_connection = mysql.connector.connect(host='localhost',user='root',password="1234",database='chatbot')
 except DatabaseError: 
@@ -12,10 +15,47 @@ except DatabaseError:
 def equation(x):
         y = (-1*(abs((x-1)/(math.e*(math.sqrt(1+pow(x-1,2)))))))+(math.e/10)
         return y
+# def checkNonDatabaseLine(user_text):
+#     with open('skill_intent.json','r') as f:
+#         json_dic = json.load(f)
+#     for i in json_dic:
+#         if user_text.lower() == i:
+#             if json_dic[i] == "news":
+#                 str_news = news()
+#                 return(str_news)
+#             elif json_dic[i] == "weather":
+#                 url = 'http://ipinfo.io/json'
+#                 response = urlopen(url)
+#                 data = json.load(response)
+#                 weather_data = weather(data['city'])
+#                 return(f"In {weather_data[2]} the current tempature is {weather_data[1]} with {weather_data[0]}")
+#             elif json_dic[i] == "time":
+#                 the_time = time()
+#                 return the_time
+#             elif json_dic[i] =="date":
+#                 the_date = date()
+#                 return the_date
+def checkNonDatabaseLine(user_text):
+    if "news" in user_text:
+        str_news = news()
+        return(str_news)
+    elif "weather" in user_text:
+        url = 'http://ipinfo.io/json'
+        response = urlopen(url)
+        data = json.load(response)
+        weather_data = weather(data['city'])
+        return(f"In {weather_data[2]} the current tempature is {weather_data[1]} with {weather_data[0]}")
+    elif "time" in user_text:
+        the_time = time()
+        return the_time
+    elif "date" in user_text:
+        the_date = date()
+        return(the_date)
 win = Tk()
 win.geometry('400x600')
 win.title("Chatbot Training")
-win.resizable(width=FALSE, height=FALSE)
+win.configure(bg='white')
+win.resizable(width=False, height=False)
 class ChatbotGUI():
     def __init__(self):
         self.current_word=None
@@ -94,13 +134,18 @@ class ChatbotGUI():
                     json_dic[self.current_word]=int(percent)
                     json.dump(json_dic,f,indent=4)
         except KeyError:
-           print("Please select the Start Conversation button to start the conversation")
+           print("Please Send a Message to dictate the response")
     def giveWord(self,user_text):
         database = getDatabase(db_connection,self.intent)
         all_words = database.getAllWords()
         known = False
+
+        if checkNonDatabaseLine(user_text.lower()):
+            return(checkNonDatabaseLine(user_text.lower()))
+        
         for i in all_words:
             if i == user_text.lower():
+                
                 known = True
                 self.intent = f'chatbot_data.{all_words[i]}'
                 GUI = ChatbotGUI()
@@ -120,8 +165,8 @@ class ChatbotGUI():
         self.current_word = random_word[0]
         return(f'{random_word[0]}: {words_factor[random_word[0]]}')   
 chatbotGUI = ChatbotGUI()
-yes_button = Button(win, text="Yes", bd=5,width=100,height=2,fg='red', command=chatbotGUI.yes)
-no_button = Button(win,text='No',bd=5,width=100,height=2, fg='blue',command=chatbotGUI.no)
+yes_button = Button(win, text="Yes", bd=5,width=100,height=2,fg='red',bg="#32de97", command=chatbotGUI.yes)
+no_button = Button(win,text='No',bd=5,width=100,height=2, fg='blue',bg="#32de97",command=chatbotGUI.no)
 no_button.pack(side='bottom')
 yes_button.pack(side='bottom')
 def send():
@@ -133,6 +178,7 @@ def send():
         ChatLog.config(foreground="black", font=("Times", 12 ))
         res = (chatbotGUI.giveWord(msg))
         ChatLog.insert(END, "Computer: " + res + '\n\n')
+        ChatLog.pack(padx=10, pady=10)
 
         ChatLog.config(state=DISABLED)
         ChatLog.yview(END)
@@ -143,9 +189,9 @@ ChatLog['yscrollcommand']=scrollbar.set
 SendButton = Button(win,font=("vedanta",12,'bold'), text="Send", width="12", height=5,
                     bd=7, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
                     command= send)
-EntryBox = Text(win,bd=0, bg="white",width="29", height="5", font="Times",)
+EntryBox = Text(win,bd=0, bg="white",width="29", height="5", font="Times",fg="black")
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=-7, y=401, height=90) 
+EntryBox.place(x=132, y=397, height=85, width=260)
+SendButton.place(x=-7, y=397, height=85) 
 win.mainloop()  
--- skill.py
+++ skill.py
@@ -1,37 +1,45 @@
+from datetime import datetime
+from inspect import Traceback
 from urllib.request import urlopen
 import requests
 from bs4 import BeautifulSoup
 import json
+import datetime 
+
+# Function to grab news
 def news():
-    main_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=96f6d9f249d3430fb7d26eba8d7cc541'
-    open_page = requests.get(main_url).json()
-    article = open_page['articles']
-    results = []
-    return_statment = ""
-    for i in article:
-        results.append(i['title'])
-    for i in range (10):
-        return_statment = return_statment+(f'{i + 1}: {results[i]}\n')
-    return return_statment
+    try:
+        main_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=96f6d9f249d3430fb7d26eba8d7cc541'
+        open_page = requests.get(main_url).json()
+        article = open_page['articles']
+        results = []
+        return_statment = ""
+        for i in article:
+            results.append(i['title'])
+        for i in range (10):
+            return_statment = return_statment+(f'{i + 1}: {results[i]}\n')
+        return return_statment
+    except:
+        return "can't connect to the internet right now please try again later"
+
+# Function to grab weather
 def weather(city):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     city= (f'{city}+weather')
     res = requests.get(f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',headers=headers)
     soup = BeautifulSoup(res.text,'html.parser')   
-    location = soup.select('#wob_loc')[0].getText().strip()  
-    # time = soup.select('#wob_dts')[0].getText().strip()       
-    info = soup.select('#wob_dc')[0].getText().strip() 
-    weather = soup.select('#wob_tm')[0].getText().strip()
-    return([info,weather+"°F",location])
-url = 'http://ipinfo.io/json'
-response = urlopen(url)
-data = json.load(response)
-IP=data['ip']
-org=data['org']
-city = data['city']
-country=data['country']
-region=data['region']
-
-weather_data = weather(data['city'])
-print(f"In {weather_data[2]} the current tempature is {weather_data[1]} with {weather_data[0]}")
-print(news())
+    try:
+        location = soup.select('#wob_loc')[0].getText().strip()  
+        # time = soup.select('#wob_dts')[0].getText().strip()       
+        info = soup.select('#wob_dc')[0].getText().strip() 
+        weather = soup.select('#wob_tm')[0].getText().strip()
+        return([info,weather+"°F",location])
+    except IndexError:
+        return("I can't get your location right now")
+#time
+def time():
+    time = datetime.datetime.now().strftime("%I:%M %p")
+    return time
+def date():
+    date = datetime.datetime.now().strftime("%A %d/%m/%Y")
+    return date
